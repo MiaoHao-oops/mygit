4e2376107e3902441a572da7abd0d38205d1860c
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2022-11-18 14:36:13 +0800

    try sha1 and create .mygit if not exist

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::{fs::{File, self, DirBuilder}, io::Read};
+use sha1::{Sha1, Digest};
+
+mod hash;
+
 fn main() {
-    println!("Hello, world!");
+    let hex_digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+    let mut f = File::open("/home/haooops/Documents/mygit/test/a.c").expect("file open error!");
+    let mut buf = String::new();
+    let mut hash = String::new();
+    f.read_to_string(&mut buf).unwrap();
+
+    let mut hasher = Sha1::new();
+    hasher.update(&buf);
+    let result = hasher.finalize();
+    for i in result {
+        hash.push(hex_digits[(i & 0xf) as usize]);
+        hash.push(hex_digits[((i & 0xf0) >> 4) as usize]);
+    }
+    println!("{}", hash);
+
+    let dir = fs::read_dir("./").expect("open current directory error!");
+    let mut has_init = false;
+
+    // find whether .mygit exists
+    for file in dir {
+        let file = file.unwrap();
+        if file.file_type().unwrap().is_dir() && file.file_name().to_str().unwrap() == ".mygit" {
+            has_init = true;
+        }
+    }
+
+    // if .mygit does not exist, create directory .mygit
+    if !has_init {
+        println!("Initialize mygit by creating directory .mygit");
+        DirBuilder::new()
+        .recursive(true)
+        .create("./.mygit").unwrap();
+    }
 }
