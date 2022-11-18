c846744eb6f7be73cdd1e3f1c28e652ae07d7d3b
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2022-11-18 16:04:39 +0800

    extract init and get sha1 into functions

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,13 @@ use sha1::{Sha1, Digest};
 mod hash;
 
 fn main() {
-    let hex_digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
-    let mut f = File::open("/home/haooops/Documents/mygit/test/a.c").expect("file open error!");
-    let mut buf = String::new();
-    let mut hash = String::new();
-    f.read_to_string(&mut buf).unwrap();
+    init();
 
-    let mut hasher = Sha1::new();
-    hasher.update(&buf);
-    let result = hasher.finalize();
-    for i in result {
-        hash.push(hex_digits[(i & 0xf) as usize]);
-        hash.push(hex_digits[((i & 0xf0) >> 4) as usize]);
-    }
+    let hash = get_sha1("/home/haooops/Documents/mygit/test/a.c");
     println!("{}", hash);
+}
 
+fn init() {
     let dir = fs::read_dir("./").expect("open current directory error!");
     let mut has_init = false;
 
@@ -32,9 +24,30 @@ fn main() {
 
     // if .mygit does not exist, create directory .mygit
     if !has_init {
-        println!("Initialize mygit by creating directory .mygit");
+        println!("Initialize mygit by creating directory ./.mygit");
         DirBuilder::new()
         .recursive(true)
         .create("./.mygit").unwrap();
+        DirBuilder::new()
+        .recursive(true)
+        .create("./.mygit/objects").unwrap();
     }
+}
+
+fn get_sha1(file_name: &str) -> String {
+    let hex_digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+    let mut f = File::open(file_name).expect("file open error!");
+    let mut buf = String::new();
+    let mut hash = String::new();
+    f.read_to_string(&mut buf).unwrap();
+
+    let mut hasher = Sha1::new();
+    hasher.update(&buf);
+    let result = hasher.finalize();
+    
+    for i in result {
+        hash.push(hex_digits[(i & 0xf) as usize]);
+        hash.push(hex_digits[((i & 0xf0) >> 4) as usize]);
+    }
+    hash
 }
