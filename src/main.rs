4e0a7b7f50fa0c62425abea2ec7d59dbcaec337a
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2022-11-19 16:16:25 +0800

    support locate repo root from subdirs, thanks to crate project-root!

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,39 @@
-use std::{fs::{File, self, DirBuilder}, io::{Read, Write}, env, process::exit};
+use std::fs::{File, self, DirBuilder};
+use std::io::{Read, Write, self, ErrorKind};
+use std::env;
+use std::process::exit;
+use std::path::PathBuf;
+use std::ffi::OsString;
 use sha1::{Sha1, Digest};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let repo_root = get_repo_root();
 
     // parse arguments
     if args.len() > 1 {
         let cmd = args[1].as_str();
         match cmd {
-            "init" => init(),
+            "init" => {
+                if let Err(_) = repo_root {
+                    // if .mygit dose not exist in the ancestors of 
+                    // current directory, initialize the repository.
+                    init();
+                }
+            },
             "add" => {
-                if !has_init() {
+                if let Err(_) = repo_root {
                     println!("this is not a mygit repository!");
                     exit(1);
-                }
-                if args.len() > 2 {
-                    add(args[2].as_str());
-                } else {
-                    println!("lack of arguments!");
-                    exit(1);
+                } else if let Ok(repo_root) = repo_root {
+                    // if .mygit exists in the ancestors of current
+                    // directory, extract it from Result<>.
+                    if args.len() > 2 {
+                        add(args[2].as_str(), repo_root.to_str().unwrap());
+                    } else {
+                        println!("lack of arguments!");
+                        exit(1);
+                    }
                 }
             },
             _ => {
@@ -32,13 +47,6 @@ fn main() {
     };
 }
 
-fn dir_exist(dir_path: &str) -> bool {
-    match fs::read_dir(dir_path) {
-        Ok(_) => true,
-        Err(_) => false
-    }
-}
-
 fn file_exist(file_path: &str) -> bool {
     match File::open(file_path) {
         Ok(_) => true,
@@ -46,21 +54,32 @@ fn file_exist(file_path: &str) -> bool {
     }
 }
 
-fn has_init() -> bool {
-    dir_exist("./.mygit")
+// thanks to crate project-root!
+fn get_repo_root() -> io::Result<PathBuf> {
+    let path = env::current_dir()?;
+    let mut path_ancestors = path.as_path().ancestors();
+
+    while let Some(p) = path_ancestors.next() {
+        let has_mygit =
+            fs::read_dir(p)?
+                .into_iter()
+                .any(|p| p.unwrap().file_name() == OsString::from(".mygit"));
+        if has_mygit {
+            return Ok(PathBuf::from(p))
+        }
+    }
+    Err(io::Error::new(ErrorKind::NotFound, "Ran out of places to find .mygit"))
 }
 
 fn init() {
-    // if .mygit does not exist, create directory .mygit
-    if !has_init() {
-        println!("Initialize mygit by creating directory ./.mygit");
-        DirBuilder::new()
-        .recursive(true)
-        .create("./.mygit").unwrap();
-        DirBuilder::new()
-        .recursive(true)
-        .create("./.mygit/objects").unwrap();
-    }
+    // create directory .mygit and sub directories
+    println!("Initialize mygit by creating directory ./.mygit");
+    DirBuilder::new()
+    .recursive(true)
+    .create("./.mygit").unwrap();
+    DirBuilder::new()
+    .recursive(true)
+    .create("./.mygit/objects").unwrap();
 }
 
 fn get_sha1(buf: &Vec<u8>) -> String {
@@ -78,7 +97,7 @@ fn get_sha1(buf: &Vec<u8>) -> String {
     hash
 }
 
-fn add(path: &str) {
+fn add(path: &str, repo_root: &str) {
     // ignore mygit metadata
     if path == "./.mygit" || path == "./target" || path == "./.git" {
         return;
@@ -92,8 +111,8 @@ fn add(path: &str) {
         let hash = String::from(get_sha1(&content));
 
         // if the blob of the file dose not exist, create it
-        if !blob_exist(&hash) {
-            mkblob(&content, &hash);
+        if !blob_exist(&hash, repo_root) {
+            mkblob(&content, &hash, repo_root);
         }
         return;
     }
@@ -105,19 +124,23 @@ fn add(path: &str) {
         let file_name = String::from(file.path().to_str().unwrap());
 
         // traverse project directory tree recursively
-        add(&file_name);
+        add(&file_name, repo_root);
     }
 }
 
-fn blob_exist(hash: &str) -> bool {
-    let blob_path = String::from("./.mygit/objects/");
-    let blob_path = blob_path + hash;
-    file_exist(blob_path.as_str())
+fn gen_blob_path(repo_root: &str, blob_name: &str) -> String {
+    let blob_path = String::from(repo_root);
+    let blob_path = blob_path + "/.mygit/objects" + blob_name;
+    blob_path
 }
 
-fn mkblob(content: &Vec<u8>, hash: &str) {
-    let blob_path = String::from("./.mygit/objects/");
-    let blob_path = blob_path + hash;
+fn blob_exist(hash: &str, repo_root: &str) -> bool {
+    let blob_path = gen_blob_path(repo_root, hash);
+    file_exist(&blob_path)
+}
+
+fn mkblob(content: &Vec<u8>, hash: &str, repo_root: &str) {
+    let blob_path = gen_blob_path(repo_root, hash);
     let mut blob = File::create(&blob_path).expect("blob create error!");
 
     blob.write("blob ".as_bytes()).unwrap();
